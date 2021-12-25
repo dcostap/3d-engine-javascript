15e022b6c07d3bb1643b9e5bac53fbb162c912a7
Dario Costa Pena <Dario Costa@CASTROSUA.COM>
2021-12-25 15:11:27 +0100

    Seems to be working

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 let width = 400;
 let height = 400;
 let aspect_ratio = height / width;
-let fov = 100;
+let fov = 90;
 let z_near = 10;
-let z_far = 100;
+let z_far = 400;
 
 class Vector {
     x;
@@ -25,6 +25,33 @@ class Mesh {
     constructor(...triangles) {
         this.triangles.push(...triangles)
     }
+
+    set_origin_to_center() {
+        let min_x = null; let max_x = null;
+        let min_y = null; let max_y = null;
+        let min_z = null; let max_z = null;
+
+        for (let triangle of this.triangles) {
+            for (let vector of [triangle.v1, triangle.v2, triangle.v3]) {
+                if (vector.x > max_x || max_x == null) max_x = vector.x
+                if (vector.x < min_x || min_x == null) min_x = vector.x
+
+                if (vector.y > max_y || max_y == null) max_y = vector.y
+                if (vector.y < min_y || min_y == null) min_y = vector.y
+
+                if (vector.z > max_z || max_z == null) max_z = vector.z
+                if (vector.z < min_z || min_z == null) min_z = vector.z
+            }
+        }
+
+        for (let triangle of this.triangles) {
+            for (let vector of [triangle.v1, triangle.v2, triangle.v3]) {
+                vector.x -= (max_x - min_x) / 2.0;
+                vector.y -= (max_y - min_y) / 2.0;
+                vector.z -= (max_z - min_z) / 2.0;
+            }
+        }
+    }
 }
 
 class Triangle {
@@ -42,12 +69,12 @@ class Triangle {
 function plane_to_triangles(v1, v2, v3, v4) {
     return [
         new Triangle(v1, v2, v4),
-        new Triangle(v4, v2, v3)
+        new Triangle(v2, v3, v4)
     ]
 }
 
 function make_cube(size) {
-    return new Mesh(
+    const mesh = new Mesh(
         // front face
         ...plane_to_triangles(
             new Vector(0, 0, 0),
@@ -71,26 +98,60 @@ function make_cube(size) {
             new Vector(size, size, size),
             new Vector(size, 0, size)
         ),
-    )
+
+        // left face
+        ...plane_to_triangles(
+            new Vector(0, 0, 0),
+            new Vector(0, size, 0),
+            new Vector(0, size, size),
+            new Vector(0, 0, size)
+        ),
+
+        // top face
+        ...plane_to_triangles(
+            new Vector(0, size, 0),
+            new Vector(0, size, size),
+            new Vector(size, size, size),
+            new Vector(size, size, 0)
+        ),
+
+        // bottom face
+        ...plane_to_triangles(
+            new Vector(0, 0, 0),
+            new Vector(0, 0, size),
+            new Vector(size, 0, size),
+            new Vector(size, 0, 0)
+        ),
+    );
+
+    // mesh.set_origin_to_center();
+
+    return mesh;
 }
 
 let meshes = [
-    make_cube(400)
+    make_cube(50)
 ]
 
 function setup() {
     createCanvas(width, height);
+
+    for (let mesh of meshes) {
+        // mesh.position.z += 10;
+    }
 }
 
 function draw() {
     background(220);
 
     for (let mesh of meshes) {
-        // mesh.rotation.y += 1
-        mesh.position.z += 1
+        mesh.position.z += 1;
+        // mesh.position.x += 1;
+        // mesh.rotation.y += 0.4;
         for (let triangle of mesh.triangles) {
             let proj = project_triangle(mesh, triangle);
-            draw_triangle(proj);
+            // if (proj.v1.z > z_near && proj.v2.z > z_near && proj.v3.z > z_near)
+                draw_triangle(proj);
         }
     }
 }
@@ -103,10 +164,10 @@ function draw_triangle(triangle) {
     noFill();
     stroke(237, 34, 93);
     strokeWeight(2);
-    vertex(triangle.v1.x, triangle.v1.y);
-    vertex(triangle.v2.x, triangle.v2.y);
-    vertex(triangle.v3.x, triangle.v3.y);
-    vertex(triangle.v1.x, triangle.v1.y);
+    vertex(triangle.v1.x, height - triangle.v1.y);
+    vertex(triangle.v2.x, height - triangle.v2.y);
+    vertex(triangle.v3.x, height - triangle.v3.y);
+    vertex(triangle.v1.x, height - triangle.v1.y);
     endShape();
 }
 
@@ -128,31 +189,43 @@ function project_triangle(mesh, triangle) {
     return result;
 }
 
+Math.cosDeg = function (angle) {
+    return Math.cos(to_radians(angle));
+}
+
+Math.sinDeg = function (angle) {
+    return Math.sin(to_radians(angle));
+}
+
 function project_vertex(mesh, vertex) {
-    result = new Vector();
+    let result = new Vector();
 
-    let transformed_x = vertex.x //* Math.cos(to_radians(mesh.rotation.y));
-    let transformed_y = vertex.y //* Math.cos(to_radians(mesh.rotation.x));
-    let transformed_z = vertex.z //* Math.sin(to_radians(mesh.rotation.y)) * Math.sin(to_radians(mesh.rotation.x));
+    let transformed_x = vertex.x * Math.cosDeg(mesh.rotation.y) - vertex.z * Math.sinDeg(mesh.rotation.y);
+    let transformed_y = vertex.y;
+    let transformed_z = vertex.z * Math.cosDeg(mesh.rotation.y) + vertex.x * Math.sinDeg(mesh.rotation.y)
 
-    // transformed_x += mesh.position.x;
-    // transformed_y += mesh.position.y;
-    // transformed_z += mesh.position.z;
+    transformed_x += mesh.position.x;
+    transformed_y += mesh.position.y;
+    transformed_z += mesh.position.z;
 
     // project from screen units to -1 -> 1
-    // transformed_x /= width;
-    // transformed_y /= height;
+    transformed_x /= width;
+    transformed_y /= height;
 
     // transformed_x *= 2; transformed_y *= 2;
     // transformed_x -= 1; transformed_y -= 1;
+
+    transformed_z /= z_far - z_near;
+    // transformed_z += 1;
 
     // inverse tangent of half of FOV
     let f = 1 / Math.tan(to_radians(fov / 2.0));
     let q = z_far / (z_far - z_near);
 
     result.x = aspect_ratio * f * transformed_x;
-    result.y = aspect_ratio * f * transformed_y;
-    result.z = (q * transformed_z) - (q * z_near);
+    console.log(transformed_x);
+    result.y = f * transformed_y;
+    result.z = transformed_z * (q - (q * z_near));
 
     if (transformed_z != 0) {
         result.x /= transformed_z;
@@ -162,8 +235,16 @@ function project_vertex(mesh, vertex) {
     // result.x += 1;
     // result.y += 1;
 
-    // result.x *= width;
-    // result.y *= height;
+    // result.x /= 2;
+    // result.y /= 2;
+
+    result.x *= width;
+    result.y *= height;
+
+    // view is centered,
+    // objects at (0, 0) are in the middle if camera is in (0, 0)
+    result.x += width / 2;
+    result.y += height / 2;
 
     return result;
 }
